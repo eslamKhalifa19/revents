a763090b8204d1f3bbbfa13d45653a8b86bcd630
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-02-25 11:59:06 +0200

    event form

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-
+import React, { useState } from "react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
 import NavBar from "../../features/events/nav/NavBar";
+import { Container } from "semantic-ui-react";
 
 export default function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
     <>
-      <NavBar />
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className="main">
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
     </>
   );
 }
