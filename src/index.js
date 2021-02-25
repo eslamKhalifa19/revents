24c818e84419f9685a8fcfd5727208ff6cdc3a6b
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-02-25 09:26:23 +0200

    install semantic-ui-react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/layout/App";
+import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import reportWebVitals from "./reportWebVitals";
 
