349a6b436f657315eae76698250cdc5166511384
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-06 00:49:52 +0200

    using navlink in navbar

--- src/features/nav/NavBar.js
+++ src/features/nav/NavBar.js
@@ -1,22 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Menu, Container, Button } from "semantic-ui-react";
 
 export default function NavBar({ setFormOpen }) {
   return (
     <Menu inverted fixed="top">
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} exact to="/" header>
           <img src="/assets/logo.png" alt="logo" style={{ marginRight: 15 }} />
           Re-vents
         </Menu.Item>
-        <Menu.Item name="Events" />
-        <Menu.Item>
-          <Button
-            onClick={() => setFormOpen(true)}
-            positive
-            inverted
-            content="Create Event"
-          />
+        <Menu.Item as={NavLink} to="/events" name="Events" />
+        <Menu.Item as={NavLink} to="/createEvent">
+          <Button positive inverted content="Create Event" />
         </Menu.Item>
         <Menu.Item position="right">
           <Button basic inverted content="Login" />
