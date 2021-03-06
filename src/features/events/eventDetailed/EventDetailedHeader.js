df28c31cb810027b2a0d049f4a7e30ecc59aec56
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-06 03:22:09 +0200

    add link

--- src/features/events/eventDetailed/EventDetailedHeader.js
+++ src/features/events/eventDetailed/EventDetailedHeader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Segment, Image, Item, Header, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const eventImageStyle = {
   filter: "brightness(30%)",
@@ -14,14 +15,14 @@ const eventImageTextStyle = {
   color: "white",
 };
 
-const EventDetailedHeader = () => {
+export default function EventDetailedHeader() {
   return (
     <Segment.Group>
       <Segment basic attached="top" style={{ padding: "0" }}>
         <Image
-          style={eventImageStyle}
           src={`/assets/categoryImages/drinks.jpg`}
           fluid
+          style={eventImageStyle}
         />
 
         <Segment basic style={eventImageTextStyle}>
@@ -47,12 +48,10 @@ const EventDetailedHeader = () => {
         <Button>Cancel My Place</Button>
         <Button color="teal">JOIN THIS EVENT</Button>
 
-        <Button color="orange" floated="right">
+        <Button as={Link} to={`/manage/`} color="orange" floated="right">
           Manage Event
         </Button>
       </Segment>
     </Segment.Group>
   );
-};
-
-export default EventDetailedHeader;
+}
