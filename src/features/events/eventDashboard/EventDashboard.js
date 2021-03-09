bc867891123895a6c27a409c086b3d0b42df0b93
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-09 04:13:46 +0200

    getting events from the redux store

--- src/features/events/eventDashboard/EventDashboard.js
+++ src/features/events/eventDashboard/EventDashboard.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
-import { sampleData } from "../../../app/api/sampleData";
+import { useSelector } from "react-redux";
 export default function EventDashboard() {
-  const [events, setEvents] = useState(sampleData);
-
+  const { events } = useSelector((state) => state.event);
   /*function handleCreateEvent(event) {
     setEvents([...events, event]);
   }
@@ -17,7 +16,7 @@ export default function EventDashboard() {
   }
 */
   function handleDeleteEvent(eventId) {
-    setEvents(events.filter((evt) => evt.id !== eventId));
+    //setEvents(events.filter((evt) => evt.id !== eventId));
   }
   return (
     <Grid>
