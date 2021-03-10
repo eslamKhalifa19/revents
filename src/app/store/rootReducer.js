eb8379ca2258eed80c442365122d9f4322557323
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-10 21:15:32 +0200

    adding an auth reducer

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import eventReducer from "../../features/events/eventReducer";
 import modalReducer from "../common/modals/modalReducer";
-
+import authReducer from "../../features/auth/authReducer";
 const rootReducer = combineReducers({
   event: eventReducer,
   modals: modalReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
