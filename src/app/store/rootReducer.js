d370fc7061481355aec9ce8554a31fda5e776839
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-10 19:19:51 +0200

    build basic modal

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import eventReducer from "../../features/events/eventReducer";
+import modalReducer from "../common/modals/modalReducer";
 
 const rootReducer = combineReducers({
   event: eventReducer,
+  modals: modalReducer,
 });
 
 export default rootReducer;
