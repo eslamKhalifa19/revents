a0b387e2cf435cbc04fe44ff8cc6eb9a3b67c2fb
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-10 19:01:17 +0200

    handle submit button behaviour

--- src/features/events/eventForm/EventForm.js
+++ src/features/events/eventForm/EventForm.js
@@ -53,38 +53,46 @@ export default function EventForm({ match, history }) {
           history.push("/events");
         }}
       >
-        <Form className="ui form">
-          <Header sub color="teal" content="Event Details" />
+        {({ isSubmitting, dirty, isValid }) => (
+          <Form className="ui form">
+            <Header sub color="teal" content="Event Details" />
+            <TextInput name="title" placeholder="Event title" />
+            <SelectInput
+              name="category"
+              placeholder="Event category"
+              options={categoryData}
+            />
+            <TextArea name="description" placeholder="Description" rows={3} />
+            <Header sub color="teal" content="Event Location Details" />
+            <TextInput name="city" placeholder="City" />
+            <TextInput name="venue" placeholder="Venue" />
+            <DateInput
+              name="date"
+              placeholderText="Event date"
+              timeFormat="HH:mm"
+              showTimeSelect
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm a"
+            />
 
-          <TextInput name="title" placeholder="Event title" />
-          <SelectInput
-            name="category"
-            placeholder="Event category"
-            options={categoryData}
-          />
-          <TextArea name="description" placeholder="Description" rows={3} />
-          <Header sub color="teal" content="Event Location Details" />
-
-          <TextInput name="city" placeholder="City" />
-          <TextInput name="venue" placeholder="Venue" />
-          <DateInput
-            name="date"
-            placeholderText="Event date"
-            timeFormat="HH:mm"
-            showTimeSelect
-            timeCaption="time"
-            dateFormat="MMM d, yyy h:mm a"
-          />
-
-          <Button type="submit" floated="right" positive content="Submit" />
-          <Button
-            as={Link}
-            to="/events"
-            type="submit"
-            floated="right"
-            content="Cancel"
-          />
-        </Form>
+            <Button
+              loading={isSubmitting}
+              disabled={!isValid || !dirty || isSubmitting}
+              type="submit"
+              floated="right"
+              positive
+              content="Submit"
+            />
+            <Button
+              disabled={isSubmitting}
+              as={Link}
+              to="/events"
+              type="submit"
+              floated="right"
+              content="Cancel"
+            />
+          </Form>
+        )}
       </Formik>
     </Segment>
   );
