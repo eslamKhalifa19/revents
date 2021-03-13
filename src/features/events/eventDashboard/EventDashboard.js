723cb4b5055779ab88b0482b87834d9e813ab75b
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-13 05:10:18 +0200

    adding an eventFilters

--- src/features/events/eventDashboard/EventDashboard.js
+++ src/features/events/eventDashboard/EventDashboard.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
 import { useSelector } from "react-redux";
-import Loading from "../../../app/layout/Loading";
 import EventListItemPlaceholder from "./EventListPlaceholder";
+import EventFilters from "./EventFilters";
 export default function EventDashboard() {
   const { events } = useSelector((state) => state.event);
   const { loading } = useSelector((state) => state.async);
@@ -20,7 +20,7 @@ export default function EventDashboard() {
         <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
-        <h2>EventFilters</h2>
+        <EventFilters />
       </Grid.Column>
     </Grid>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./app/layout/App";
 import "semantic-ui-css/semantic.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import "./app/layout/styles.css";
+import "react-calendar/dist/Calendar.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
