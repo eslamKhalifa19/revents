1712028bcffe4b166e588da4b03f7cfc8800b386
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-13 04:38:04 +0200

    adding a mock api  async fetchEvents

--- src/features/events/eventActions.js
+++ src/features/events/eventActions.js
@@ -1,5 +1,27 @@
-import { CREATE_EVENT, DELETE_EVENT, UPDATE_EVENT } from "./eventConstants";
-
+import { fetchSampleData } from "../../app/api/mockApi";
+import {
+  asyncActionsError,
+  asyncActionsFinish,
+  asyncActionsStart,
+} from "../../app/async/asyncReducer";
+import {
+  CREATE_EVENT,
+  DELETE_EVENT,
+  FETCH_EVENTS,
+  UPDATE_EVENT,
+} from "./eventConstants";
+export function loadEvents() {
+  return async function (dispatch) {
+    dispatch(asyncActionsStart());
+    try {
+      const events = await fetchSampleData();
+      dispatch({ type: FETCH_EVENTS, payload: events });
+      dispatch(asyncActionsFinish());
+    } catch (error) {
+      dispatch(asyncActionsError(error));
+    }
+  };
+}
 export function createEvent(event) {
   return {
     type: CREATE_EVENT,
