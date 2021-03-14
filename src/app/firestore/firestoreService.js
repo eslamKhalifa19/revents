912a3d162d8a1090724840268a0da01a7909944b
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-15 01:17:05 +0200

    shaping the firestore data

--- src/app/firestore/firestoreService.js
+++ src/app/firestore/firestoreService.js
@@ -2,6 +2,24 @@ import firebase from "../config/firebase";
 
 const db = firebase.firestore();
 
+export function dataFromSnapshot(snapshot) {
+  if (!snapshot.exists) return undefined;
+  const data = snapshot.data();
+
+  for (const prop in data) {
+    if (data.hasOwnProperty(prop)) {
+      if (data[prop] instanceof firebase.firestore.Timestamp) {
+        data[prop] = data[prop].toDate();
+      }
+    }
+  }
+
+  return {
+    ...data,
+    id: snapshot.id,
+  };
+}
+
 export function getEventsFromFirestore(observer) {
   return db.collection("events").onSnapshot(observer);
 }
--- src/features/events/eventDashboard/EventDashboard.js
+++ src/features/events/eventDashboard/EventDashboard.js
@@ -1,22 +1,31 @@
 import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import EventListItemPlaceholder from "./EventListPlaceholder";
 import EventFilters from "./EventFilters";
-import { getEventsFromFirestore } from "../../../app/firestore/firestoreService";
+import {
+  dataFromSnapshot,
+  getEventsFromFirestore,
+} from "../../../app/firestore/firestoreService";
+import { listenToEvents } from "../eventActions";
 export default function EventDashboard() {
+  const dispatch = useDispatch();
   const { events } = useSelector((state) => state.event);
   const { loading } = useSelector((state) => state.async);
 
   useEffect(() => {
     const unsubscribe = getEventsFromFirestore({
       next: (snapshot) =>
-        console.log(snapshot.docs.map((docSnapshot) => docSnapshot.data())),
+        dispatch(
+          listenToEvents(
+            snapshot.docs.map((docSnapshot) => dataFromSnapshot(docSnapshot))
+          )
+        ),
       error: (error) => console.log(error),
     });
     return unsubscribe;
-  });
+  }, [dispatch]);
   return (
     <Grid>
       <Grid.Column width={10}>
