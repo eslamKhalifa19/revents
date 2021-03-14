304f9e1fe6c2f0c703e56552a48276ac48f4bc24
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-15 00:09:37 +0200

    setting up firestore

---
+++ src/app/config/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/database";
+import "firebase/auth";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBs8EJWIatfhdP2QyrkwDR6EdE8cWqnCFA",
+  authDomain: "revents-d47ea.firebaseapp.com",
+  projectId: "revents-d47ea",
+  storageBucket: "revents-d47ea.appspot.com",
+  messagingSenderId: "616710303554",
+  appId: "1:616710303554:web:4c4a0321c662789aff22db",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
