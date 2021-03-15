043e060678fee47663fc13c0d6b90214b3b87c0b
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-16 00:42:55 +0200

    fix typos in eventDetailedpage

--- src/features/events/eventDetailed/EventDetailedPage.js
+++ src/features/events/eventDetailed/EventDetailedPage.js
@@ -4,30 +4,31 @@ import EventDetailedHeader from "./EventDetailedHeader";
 import EventDetailedInfo from "./EventDetailedInfo";
 import EventDetailedChat from "./EventDetailedChat";
 import EventDetailedSidebar from "./EventDetailedSidebar";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import useFirestoreDoc from "../../../app/hooks/useFirestoreDoc";
 import { listenToEventFromFirestore } from "../../../app/firestore/firestoreService";
-import Loading from "../../../app/layout/Loading";
-import { Redirect } from "react-router";
 import { listenToEvents } from "../eventActions";
+import { Redirect } from "react-router-dom";
+import Loading from "../../../app/layout/Loading";
+
 export default function EventDetailedPage({ match }) {
   const dispatch = useDispatch();
   const event = useSelector((state) =>
     state.event.events.find((e) => e.id === match.params.id)
   );
-
   const { loading, error } = useSelector((state) => state.async);
 
   useFirestoreDoc({
     query: () => listenToEventFromFirestore(match.params.id),
     data: (event) => dispatch(listenToEvents([event])),
-    dep: [match.params.id, dispatch],
+    deps: [match.params.id, dispatch],
   });
 
-  if (loading || (!event && error))
-    return <Loading content="Loading event ..." />;
+  if (loading || (!event && !error))
+    return <Loading content="Loading event..." />;
 
   if (error) return <Redirect to="/error" />;
+
   return (
     <Grid>
       <Grid.Column width={10}>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app/layout/App";
 import "semantic-ui-css/semantic.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
-import "./app/layout/styles.css";
 import "react-calendar/dist/Calendar.css";
-import reportWebVitals from "./reportWebVitals";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "./app/store/configureStore";
@@ -34,8 +33,3 @@ if (module.hot) {
 }
 
 render();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
