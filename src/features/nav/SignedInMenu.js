a6d2ccde7c47a4f81ed9075fd0f13d9ef42191a6
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-19 08:28:13 +0200

    adding additional user info into the auth reducer

--- src/features/nav/SignedInMenu.js
+++ src/features/nav/SignedInMenu.js
@@ -24,7 +24,7 @@ export default function SignedInMenu() {
         spaced="right"
         src={currentUser.photoURL || "/assets/user.png"}
       />
-      <Dropdown pointing="top left" text={currentUser.email}>
+      <Dropdown pointing="top left" text={currentUser.displayName}>
         <Dropdown.Menu>
           <Dropdown.Item
             as={Link}
