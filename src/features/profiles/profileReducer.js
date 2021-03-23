8eea8932e3f58aadca15d692b45d3ca694448eb4
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-23 05:30:56 +0200

    commit

--- src/features/profiles/profileReducer.js
+++ src/features/profiles/profileReducer.js
@@ -14,7 +14,10 @@ export default function profileReducer(
 ) {
   switch (type) {
     case LISTEN_TO_CURRENT_USER_PROFILE:
-      return { ...state, currentUserProfile: payload };
+      return {
+        ...state,
+        currentUserProfile: payload,
+      };
     case LISTEN_TO_SELECTED_USER_PROFILE:
       return {
         ...state,
