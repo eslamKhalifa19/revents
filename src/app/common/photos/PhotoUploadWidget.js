8d3691ef883272040f5f7c87cdd668e5b37e990c
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-03-25 14:31:19 +0200

    react dropZone

--- src/app/common/photos/PhotoUploadWidget.js
+++ src/app/common/photos/PhotoUploadWidget.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Header } from "semantic-ui-react";
+import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
 
 export default function PhotoUploadWidget() {
+  const [files, setFiles] = useState([]);
   return (
     <Grid>
       <Grid.Column width={4}>
         <Header color="teal" sub content="Step 1 - Add Photo" />
+        <PhotoWidgetDropzone setFiles={setFiles} />
+      </Grid.Column>
+      <Grid.Column width={1} />
+      <Grid.Column width={4}>
+        <Header color="teal" sub content="Step 2 - Resize" />
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
